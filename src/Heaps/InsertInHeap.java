9f84b244150d8f7c0dc8045af3daec1c57f6c846
kaif <shaikhkaif3258@gmail.com>
2026-01-12 23:14:54 +0530

    changes in insertInHeap class

--- src/Heaps/InsertInHeap.java
+++ src/Heaps/InsertInHeap.java
@@ -1,5 +1,32 @@
 package Heaps;
 
-public class InsertInHeap {
+import java.util.ArrayList;
 
+public class InsertInHeap {
+	static class Heap{ 
+		ArrayList<Integer> arr = new ArrayList<>();
+		
+		public void add(int data) {   //O(logn)
+			arr.add(data);
+			
+			int x = arr.size() - 1;  // child index
+			int par = (x - 1)/2; //parent index
+			
+			while(arr.get(x) < arr.get(par)) {
+				int temp = arr.get(x);
+				arr.set(x, arr.get(par));
+				arr.set(par, temp);
+				
+				x = par;
+				par = (x - 1) / 2;
+			}
+		}
+		
+		public int peek() {
+			return arr.get(0);
+		}
+	}
+	public static void main(String []args) {
+		
+	}
 }
