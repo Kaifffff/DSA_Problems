f8ab1c96cb76e0a7247988e111828deb8856d6f2
kaif <shaikhkaif3258@gmail.com>
2026-03-20 14:57:10 +0530

    Letter Combinations of a Phone Number

---
+++ src/StringPrograms/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,36 @@
+package StringPrograms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LetterCombinationsOfAPhoneNumber {
+	
+	public static List<String> letterCombinations(String digits){
+		
+		if (digits.isEmpty())
+		      return new ArrayList<>();
+
+		    List<String> ans = new ArrayList<>();
+		    ans.add("");
+		    String[] digitToLetters = {"",    "",    "abc",  "def", "ghi",
+		                                     "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+		    for (char d : digits.toCharArray()) {
+		      List<String> temp = new ArrayList<>();
+		      for (String s : ans)
+		        for (char c : digitToLetters[d - '0'].toCharArray())
+		          temp.add(s + c);
+		      ans = temp;
+		    }
+
+		    return ans;
+	}
+	
+	
+	
+	
+	public static void main(String []args) {
+		String digits = "23";
+		System.out.println(letterCombinations(digits));
+	}
+}
